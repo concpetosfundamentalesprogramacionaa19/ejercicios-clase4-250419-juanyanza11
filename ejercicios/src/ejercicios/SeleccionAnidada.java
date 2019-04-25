382e6296e610e87bf030860da857b77cfb3342f1
juanyanza11 <ww4fjj@gmail.com>
2019-04-25 12:22:29 -0500

    Subiedo Seleccion anidada

---
+++ ejercicios/src/ejercicios/SeleccionAnidada.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+import paquetedos.*;
+/**
+ *
+ * @author ADMIN-HP
+ */
+public class SeleccionAnidada {
+    
+    public static void main (String[] args){
+    
+    int calificacion = 50;
+    String exc= Operacion2.excelente;
+    String mb= Operacion2.muybuena;
+    String reg= Operacion2.regular;
+    String rep= Operacion2.reprobado;
+        if (calificacion>=90){
+        System.out.printf("Usted está aprobado (excelente) con %d\n",calificacion);
+        System.out.printf("%s %d\n",exc,calificacion);
+        
+    }else{
+            if(calificacion <90 && calificacion >=80){
+                System.out.printf("Usted está aprobado (muy buena) con %d\n",calificacion);
+                System.out.printf("%s %d\n",mb,calificacion);
+            }else{
+                if (calificacion<80 && calificacion >=50){
+                    System.out.printf("Usted está aprobado (regular) con %d\n",calificacion);
+                    System.out.printf("%s %d\n",reg,calificacion);
+                }else{
+                    System.out.printf("Usted está reprobado con %d\n");
+                    System.out.printf("%s %d\n",rep,calificacion);
+                }
+            }
+        }
+    }
+}
---
+++ ejercicios/src/paquetedos/Operacion2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquetedos;
+import ejercicios.*;
+/**
+ *
+ * @author ADMIN-HP
+ */
+public class Operacion2 {
+    
+
+    public static String excelente = "Usted está aprobado (excelente)";
+    public static String muybuena = "Usted está aprobado (muy buena)";
+    public static String regular= "Usted está aprobado (muy buena)";
+    public static String reprobado="Usted está reprobado";
+    
+   }
+
